09936a43392ad0d7013c540cdfa67d9e04c22c90
SALEH Sherif <sherifsaleh111@gmail.com>
2017-05-01 22:45:45 +0200

    Going to Heroku

--- dist/server/config/db.js
+++ dist/server/config/db.js
@@ -1,6 +1,7 @@
 "use strict";
 var config = {
-    url: 'mongodb://localhost:27017/test'
+    //url: 'mongodb://localhost:27017/test'
+    url: 'mongodb://dict:1234@ds127801.mlab.com:27801/dictionaries'
 };
 exports.__esModule = true;
 exports["default"] = config;
